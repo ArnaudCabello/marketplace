050fe06e70c98cfa7404e8c6b82e380c7284270c
Arnaud Cabello <petitarnoj@gmail.com>
2024-02-07 13:43:54 -0500

    added explicit wait

--- scrape.py
+++ scrape.py
@@ -2,34 +2,39 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 import time
 
+def elementWait(xPATH, driver, time):
+    element = WebDriverWait(driver, time).until(EC.presence_of_element_located((By.XPATH, xPATH)))
+        
 def scrape(id):
     url = 'https://www.ubisoft.com/en-us/game/rainbow-six/siege/marketplace?route=buy%2Fitem-details&itemId=' + id
     driver.get(url)
-    time.sleep(5)
+    elementWait('/html/body/div[1]/div[5]/div/div/ubisoft-connect', driver, 60)
 
     shadow_root = driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[5]/div/div/ubisoft-connect').shadow_root
     shadow_host = shadow_root.find_element(by= By.CSS_SELECTOR, value = 'iframe:nth-child(2)')
     driver.switch_to.frame(shadow_host)
 
-    last_sold_at_scrape = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[1]/span[2]/div/div/div[2]/span')
+    last_sold_at_xPATH = '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[1]/span[2]/div/div/div[2]/span'
+    elementWait(last_sold_at_xPATH, driver, 60)
+    last_sold_at_scrape = driver.find_element(by=By.XPATH, value= last_sold_at_xPATH)
     last_sold_at = last_sold_at_scrape.text
-    #print(last_sold_at)
-    time.sleep(2)
-
-    price_range_scrape = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span[2]/div/div/div[2]/span')
+ 
+    price_range_xPATH = '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[2]/span[2]/div/div/div[2]/span'
+    elementWait(price_range_xPATH, driver, 60)
+    price_range_scrape = driver.find_element(by=By.XPATH, value=price_range_xPATH)
     price_range_min = price_range_scrape.text.split()[0]
     price_range_max = price_range_scrape.text.split()[2]
-    #print(price_range_min)
-    #print(price_range_max)
-    time.sleep(2)
 
-    sale_orders_scrape = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[3]')
+    sale_orders_xPATH = '/html/body/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[4]/div[1]/div/div/div[2]/div/div[2]/div[2]/div[2]/div[3]'
+    elementWait(sale_orders_xPATH, driver, 60)
+    sale_orders_scrape = driver.find_element(by=By.XPATH, value= sale_orders_xPATH)
     sale_orders = sale_orders_scrape.text.splitlines()[1]
-    #print(sale_orders)
-    time.sleep(2)
 
     #print('no error')
 
@@ -39,7 +44,7 @@ def scrape(id):
 
 
 # Path to the downloaded ChromeDriver executable
-PATH = "C:\Program Files (x86)\chromedriver.exe"
+PATH = "chromedriver.exe"
 
 # Create a Chrome browser instance
 service = Service(executable_path= PATH)
@@ -47,12 +52,13 @@ driver = webdriver.Chrome(service= service)
 
 # Navigate to a website
 driver.get('https://www.ubisoft.com/en-us/game/rainbow-six/siege/marketplace?route=buy')
-time.sleep(4)
+
+elementWait('/html/body/div[1]/div[5]/div/div/div[1]/div/div/div[2]/button', driver, 10)
 
 driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[5]/div/div/div[1]/div/div/div[2]/button').click()
 driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[5]/div/div/div[1]/div/div/div[2]/button').click()
 
-time.sleep(2)
+time.sleep(5)
 
 currHand = driver.current_window_handle
 
@@ -68,14 +74,6 @@ for handle in handles:
     
     driver.switch_to.window(currHand)
 
-time.sleep(3)
-
-#res = scrape('0858f2ee-a91a-4236-94a3-33fb7f5c7807')
-#print(res)
-
-
-
-time.sleep(2)
 
 
 
